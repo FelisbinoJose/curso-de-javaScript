bd963aec79da4affcc62cdcfd7b084b705f8a657
FelisbinoJose <246760.jose@unibave.net>
2025-10-26 15:46:33 -0300

    Feat: adiciona conteudo sobre variaveis

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+//Declaração de variavel
+idade = 22;
+
 //Imprimi mensagem no terminal
-console.log("Hello word");
+console.log("Eu possuo " + idade + " anos");
+
+//Declaração de variavel
+idade = 23;
+
+//Imprimi mensagem no terminal
+console.log("Ano que vem eu terei " + idade + " anos");
