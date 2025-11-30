de55fd1de9f16ff2b65354515d870671b90ba23f
FelisbinoJose <246760.jose@unibave.net>
2025-11-30 19:44:10 -0300

    feat: adiciona conteúdo sobre operadores de comparação

---
+++ operadoresComparacao.js
@@ -0,0 +1,35 @@
+const idadeVinny = 36;
+const minhaIdade = 35;
+
+
+const saoIguis = idadeVinny == minhaIdade;
+
+console.log('São iguais?', saoIguis);
+
+const maiorQue = minhaIdade > idadeVinny;
+
+console.log('Minha idade é maior:', maiorQue);
+
+const menorQue = minhaIdade < idadeVinny;
+
+console.log('Minha idade é menor:', menorQue);
+
+const maiorDeIdade = minhaIdade >= 18;
+
+console.log('Maior de idade:', maiorDeIdade);
+
+const numeroUm = "10";
+const numeroDois = "11";
+
+const comparacaoEscrita = numeroUm === numeroDois;
+
+console.log('São estritamente iguais?', comparacaoEscrita);
+
+const diferencaEscrita = numeroUm !== numeroDois;
+
+console.log('São estritamente diferentes?', diferencaEscrita);
+
+const saoDiferentes = idadeVinny != minhaIdade;
+
+console.log('São diferentes?', saoDiferentes);
+
