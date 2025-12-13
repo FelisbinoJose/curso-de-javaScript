d00ec2cd5c0ffea20be943a0da3b137e5f6d64c5
FelisbinoJose <246760.jose@unibave.net>
2025-12-13 20:04:44 -0300

    feat: adiciona conteúdo sobre if e else

---
+++ condicionais.js
@@ -0,0 +1,16 @@
+// Entre 9 e 10: Excelente
+// Entre 7 e 8: Bom
+// Entre 4 e 6: Médio
+// Entre 0 e 3: Ruim
+
+const notaDoAluno = 4;
+
+if (notaDoAluno >= 9) {
+    console.log("Excelente");
+} else if (notaDoAluno >= 7) {
+    console.log("Bom");
+} else if (notaDoAluno >= 4) {
+    console.log("Médio");
+} else {
+    console.log("Ruim");
+}
