6161d45069c890dcda4670e31e7a882b5831f837
FelisbinoJose <246760.jose@unibave.net>
2025-12-14 12:35:50 -0300

    feat: adiciona conteúdo sobre o laço while

---
+++ lacoWhile.js
@@ -0,0 +1,13 @@
+let contador = 1;
+
+while (contador <= 10) {
+    console.log(contador)
+    contador++;
+}
+
+let numero = 0;
+
+do {
+    console.log("Esse é um", numero);
+    numero++;
+} while (numero <= 10)
