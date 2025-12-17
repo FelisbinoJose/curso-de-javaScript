b4e8c5d15a7be806f085f834e4e09160369d86c2
FelisbinoJose <246760.jose@unibave.net>
2025-12-17 00:15:59 -0300

    feat: adiciona entrada de dados com condições

--- entradaTerminal.js
+++ entradaTerminal.js
@@ -1,5 +1,7 @@
 const readLine = require('readline');
 
+
+
 const leitor = readLine.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -7,7 +9,19 @@ const leitor = readLine.createInterface({
 
 leitor.question('Qual é o seu nome? ', (nome) => {
     console.log('Olá', nome)
+    console.log("Bem vido ao nosso sistema!")
+
+    leitor.question('Qual é a sua idade? ', (idade) => {
+
+        if (idade > 18) {
+            console.log("Uau! Você já pode tirar a sua CNH!");
+        } else {
+            console.log("Você ainda não pode tirar a sua CNH");
+        }
+
+        leitor.close()
+
+    })
 
 
-    leitor.close()
 })
