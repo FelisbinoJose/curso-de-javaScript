57740e0da275dc46cd07111e57ba81ba785cab2f
FelisbinoJose <246760.jose@unibave.net>
2025-12-21 13:13:05 -0300

    feat: adiciona conteúdo sobre deletar intens do  array

--- arrays.js
+++ arrays.js
@@ -1,7 +1,8 @@
 const frutas = ['Uva', 'Banana', 'Maça'];
 
 console.log(frutas);
-console.log('Primeira fruta', frutas[0]);
+
+/*console.log('Primeira fruta', frutas[0]);
 console.log('Ultima fruta:', frutas[2]);
 
 console.log('Total de frutas:', frutas.length);
@@ -10,3 +11,8 @@ frutas.push('Pera');
 
 console.log('Total de frutas depois de adicionado a Pera:', frutas.length);
 console.log('Ultima fruta:', frutas[frutas.length - 1])
+*/
+
+frutas.splice(2, 1);
+
+console.log('Frutas depois de removida o ultimo intem:', frutas);
