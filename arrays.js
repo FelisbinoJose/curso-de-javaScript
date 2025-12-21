1d428ced65cfd4505922b6e2e479d1def988ac6f
FelisbinoJose <246760.jose@unibave.net>
2025-12-21 13:04:40 -0300

    docs: remove readme da branch main

---
+++ arrays.js
@@ -0,0 +1,12 @@
+const frutas = ['Uva', 'Banana', 'Maça'];
+
+console.log(frutas);
+console.log('Primeira fruta', frutas[0]);
+console.log('Ultima fruta:', frutas[2]);
+
+console.log('Total de frutas:', frutas.length);
+
+frutas.push('Pera');
+
+console.log('Total de frutas depois de adicionado a Pera:', frutas.length);
+console.log('Ultima fruta:', frutas[frutas.length - 1]);
